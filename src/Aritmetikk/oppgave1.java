04a26d8187de5488130594419bd831ed717cdad9
huseyindavut <saskin@protonmail.com>
2023-10-09 14:10:03 +0200

    se

--- src/Aritmetikk/oppgave1.java
+++ src/Aritmetikk/oppgave1.java
@@ -9,17 +9,13 @@ public class oppgave1{
 
             String navn = JOptionPane.showInputDialog("Skriv et navn");
             String innAlder = JOptionPane.showInputDialog("Skriv inn alderen");
+            int alder = Integer.parseInt(innAlder);
+            int fødselsår = 2023 - alder;
+            int pensjonsår = fødselsår + 67;
 
+            System.out.print(navn+" er i dag "+alder+" år. Han er født i "+fødselsår+".");
+            System.out.print(" I "+pensjonsår+" er han 67 år og da vil han være pensjonist");
 
-
-
-            //        String navn = showInputDialog("Skriv inn navnet:");
-            //        String innAlder = showInputDialog("Skriv inn alderen:");
-            //        int alder = Integer.parseInt(innAlder);
-            //        int fødselsår = 2020 - alder;
-            //        int pensjonsår = fødselsår + 67;
-            //        System.out.print(navn + " er i dag " + alder + " år.Han er født i " + fødselsår);
-            //        System.out.print(". I " + pensjonsår + " er han 67 år og da vil han være pensjonist.");
         }
 }
 
