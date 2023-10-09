996dd7f95016f502f331ee60d92495e4b7bcd8ff
huseyindavut <saskin@protonmail.com>
2023-10-09 13:59:13 +0200

    Notater

---
+++ src/Aritmetikk/oppgave1.java
@@ -0,0 +1,25 @@
+package Aritmetikk;
+
+import javax.swing.*;
+
+public class oppgave1{
+        public static void main(String[] args) {
+            //Les inn to variabler; navn, alder. Skriv så følgende historie (et eksempel):
+            // Per Olsen er i dag 32 år. Han er født i 1986. I 2053 er han 67 år og da vil han være pensjonist.
+
+            String navn = JOptionPane.showInputDialog("Skriv et navn");
+            String innAlder = JOptionPane.showInputDialog("Skriv inn alderen");
+
+
+
+
+            //        String navn = showInputDialog("Skriv inn navnet:");
+            //        String innAlder = showInputDialog("Skriv inn alderen:");
+            //        int alder = Integer.parseInt(innAlder);
+            //        int fødselsår = 2020 - alder;
+            //        int pensjonsår = fødselsår + 67;
+            //        System.out.print(navn + " er i dag " + alder + " år.Han er født i " + fødselsår);
+            //        System.out.print(". I " + pensjonsår + " er han 67 år og da vil han være pensjonist.");
+        }
+}
+
