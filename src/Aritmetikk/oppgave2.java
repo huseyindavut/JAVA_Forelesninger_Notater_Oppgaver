0d6fa264e7ad74c9cb1508dfb1d7c563ca569024
huseyindavut <saskin@protonmail.com>
2023-10-09 17:04:06 +0200

    oppgave 2 ,3

--- src/Aritmetikk/oppgave2.java
+++ src/Aritmetikk/oppgave2.java
@@ -16,6 +16,3 @@ public class oppgave2 {
 
     }
 }
-
-//Lag et program som definerer en variabel int antall=0.
-// Skriv så ut partallene mellom 0 og 10 (2,4,6,8) ved å øke variabelen med 2 mellom hver gang.
---
+++ src/Aritmetikk/oppgave3.java
@@ -0,0 +1,10 @@
+package Aritmetikk;
+
+public class oppgave3 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//Lag et program som regner ut arealet av et rektangel. Programmet skal lese inn lengden og bredden på rektangelet.
+// Regn så ut  arealet og skriver ut svaret på følgende måte: ”Et rektangel med bredde 20.0 cm og lengde 40.0 cm har et areal på 800.0 cm^2”.
